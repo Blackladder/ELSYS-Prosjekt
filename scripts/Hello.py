0eebdfa6ec549f6da044b3eb07fea8a85a450d8a
Blackladder <blackladder0@gmail.com>
2018-02-13 14:10:56 +0100

    Rensket opp

--- scripts/Hello.py
+++ scripts/Hello.py
@@ -1,13 +1,14 @@
 # -- coding: utf-8 -- 
 import time
 from naoqi import ALProxy
-tts = ALProxy("ALTextToSpeech", "192.168.43.100", 9559)
+IP = "192.168.43.100"
+Port = 9559
+tts = ALProxy("ALTextToSpeech", IP, Port)
 tts.setLanguage("Norwegian")'
-asr = ALProxy("ALSpeechRecognition", "192.168.43.100", 9559)
-asr.setLanguage("Norwegian")
-faceProxy = ALProxy("ALFaceDetection", "192.168.43.100", 9559)
-memoryProxy = ALProxy("ALMemory", "192.168.43.100", 9559)
-
+#asr = ALProxy("ALSpeechRecognition", IP, Port)
+#asr.setLanguage("Norwegian")
+faceProxy = ALProxy("ALFaceDetection", IP, Port)
+memoryProxy = ALProxy("ALMemory", IP, Port)
 period = 500
 faceProxy.subscribe("Test_Face", period, 0.0 )
 memValue = "FaceDetected"
@@ -15,11 +16,9 @@ for i in range(0, 1):
 	time.sleep(0.5)
 	val = memoryProxy.getData(memValue)
 	if(val!=[]):
-		if(((len(val[1])-1)==1)):
+		if(len(val[1])==2):
 			name = val[1][0][1][2]
 			print "***"
-			#print(val)
-			#print(val[1][0][1][2])
 			if(name):
 				print(name)
 				tts.say("Hei, " + name)
@@ -44,5 +43,5 @@ for i in range(0, 1):
 # Unsubscribe the module.
 faceProxy.unsubscribe("Test_Face")
 
-print "Test terminated successfully."
+print "<Hello> terminated successfully."
 
