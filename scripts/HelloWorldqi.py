d9aa8cfdc598d38ccbf050ce74ca21d005e8980e
Blackladder <36195712+Blackladder@users.noreply.github.com>
2018-02-23 08:59:22 +0100

    Create HelloWorldqi.py

---
+++ scripts/HelloWorldqi.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 -- 
+import time
+import qi
+IP = "192.168.43.100"
+Port = 9559
+ses = qi.Session("tcp://" + IP + ":" + str(Port))
+tts = ses.service("ALTextToSpeech")
+tts.setLanguage("Norwegian")
+asr = ses.service("ALSpeechRecognition")
+asr.setLanguage("Norwegian")
+#faceProxy = ALProxy("ALFaceDetection", IP, Port)
+memoryProxy = ses.service("ALMemory")
+
+tts.say("hei, verden")
+
+asr.unsubscribe("Test_ASR")
